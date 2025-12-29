7abbdc36caab888ae0fb182eaea2ea187b49d506
dongggggggg <dlehdrb020@gmail.com>
2025-12-30 02:52:12 +0900

    [이동규] restrict mode 제한

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  reactStrictMode: false, // WebSocket 연결 안정성을 위해 비활성화
   output: "standalone", // 핵심: 빌드 결과물을 최소화하여 standalone 폴더에 생성
 };
 
