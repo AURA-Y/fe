1f9109284f0aed030b026bf930da107c4a091329
신지웅 <gombang8047@gmail.com>
2025-12-30 21:30:56 +0900

    [신지웅] fix : 채팅 켰을때만 되는거 수정

--- src/components/room/LiveKitView.tsx
+++ src/components/room/LiveKitView.tsx
@@ -23,18 +23,17 @@ const RoomContent = () => {
     <>
       <div style={{ display: "flex", flex: 1, overflow: "hidden" }}>
         <VideoGrid />
-        {showChat && (
-          <div
-            style={{
-              width: "320px",
-              height: "100%",
-              borderLeft: "1px solid #333",
-              backgroundColor: "#0e0e0e",
-            }}
-          >
-            <Chat style={{ width: "100%", height: "100%" }} />
-          </div>
-        )}
+        <div
+          style={{
+            width: "320px",
+            height: "100%",
+            borderLeft: "1px solid #333",
+            backgroundColor: "#0e0e0e",
+            display: showChat ? "block" : "none",
+          }}
+        >
+          <Chat style={{ width: "100%", height: "100%" }} />
+        </div>
       </div>
       <ControlBar controls={{ chat: true }} />
       <RoomAudioRenderer />
